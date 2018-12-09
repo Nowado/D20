d25468d2e7a5018443304ac3fede51e5d7116eb1
Nowado <spam@jakubkrolikowski.pl>
2018-12-09 17:39:43 +0100

    fixed interface bugs

--- Camera2imgsets.py
+++ Camera2imgsets.py
@@ -8,13 +8,13 @@ def take_pictures(d,sets_delay,photo_delay,photo_per_side):
     camera.resolution = (1024, 768)
     for side in range(d):
         for j in range(sets_delay):
-            print('New set of '+str(side) +' side in ' + str(sets_delay - j))
+            print('New set of '+str(side+1) +' side in ' + str(sets_delay - j))
             sleep(1)
         for photo in range(photo_per_side):
             for i in range(photo_delay):
                 print('Photo in ' + str(photo_delay-i))
                 sleep(1)
-            camera.capture(str(side)+'_'+str(photo)+'.jpg', use_video_port=True)
+            camera.capture(str(side+1)+'_'+str(photo)+'.jpg', use_video_port=True)
 
 #def capture_set(names,delay):
 #    for i in names:
