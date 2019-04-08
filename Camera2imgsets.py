d4e247d46630e3c20caf768d13ab6d898600e4f2
Nowado <spam@jakubkrolikowski.pl>
2019-04-08 12:37:04 +0200

    Update to photo part

--- Camera2imgsets.py
+++ Camera2imgsets.py
@@ -9,11 +9,13 @@ def take_pictures(d,sets_delay,photo_delay,photo_per_side):
     for side in range(d):
         for j in range(sets_delay):
             print('New set of '+str(side+1) +' side in ' + str(sets_delay - j))
-            sleep(1)
+            a=input()
+            #sleep(1)
         for photo in range(photo_per_side):
             for i in range(photo_delay):
                 print('Photo in ' + str(photo_delay-i))
-                sleep(1)
+                #sleep(1)
+                a=input()
             camera.capture(str(side+1)+'_'+str(photo)+'.jpg', use_video_port=True)
 
 #def capture_set(names,delay):
